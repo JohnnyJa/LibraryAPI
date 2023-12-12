af162cb49597efb0a855c6d3e7f40f78da98b941
Иван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2023-12-13 00:40:20 +0200

    DAL tests added

---
+++ DAL.UnitTest/Factory/TestApplicationDBContextFactory.cs
@@ -0,0 +1,104 @@
+using Library.DAL.Contexts;
+using Library.DAL.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.UnitTest.Factory;
+
+public class TestApplicationDbContextFactory
+{
+    public ApplicationDbContext CreateFilledDbContext()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options;
+
+        var context = new ApplicationDbContext(optionsBuilder);
+
+        if (context.Database.EnsureCreated())
+        {
+            using var viewCommand = context.Database.GetDbConnection().CreateCommand();
+            viewCommand.CommandText = @"
+CREATE VIEW AllResources AS
+SELECT Name
+FROM Author;";
+            viewCommand.ExecuteNonQuery();
+        }
+
+        var authors = new[]
+        {
+            new Author { Name = "Author1", Surname = "Author1" },
+            new Author { Name = "Author2", Surname = "Author2" }
+        };
+
+        var books = new[]
+        {
+            new Book()
+            {
+                Name = "Book1", ISBN = "123", Author = authors[0]
+            },
+            new Book()
+            {
+                Name = "Book2", ISBN = "1234", Author = authors[1]
+            }
+        };
+        
+        var formularies = new[]
+        {
+            new ReaderFormulary()
+            {
+                Name = "Name1", Surname = "Surname1"
+            },
+            new ReaderFormulary()
+            {
+                Name = "Book2", Surname = "Surname1"
+            }
+        };
+        
+        formularies[0].TakenBooks.Add(books[0]);
+        formularies[1].TakenBooks.Add(books[1]);
+        
+        
+
+
+        context.AddRange(authors);
+        context.AddRange(books);
+        context.AddRange(formularies);
+        context.SaveChanges();
+
+        return context;
+    }
+    
+    public ApplicationDbContext CreateAuthorFilledDbContext()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options;
+
+        var context = new ApplicationDbContext(optionsBuilder);
+
+        if (context.Database.EnsureCreated())
+        {
+            using var viewCommand = context.Database.GetDbConnection().CreateCommand();
+            viewCommand.CommandText = @"
+CREATE VIEW AllResources AS
+SELECT Name
+FROM Author;";
+            viewCommand.ExecuteNonQuery();
+        }
+
+        var authors = new[]
+        {
+            new Author { Name = "Author1", Surname = "Author1" },
+            new Author { Name = "Author2", Surname = "Author2" }
+        };
+
+        context.AddRange(authors);
+        context.SaveChanges();
+
+        return context;
+    }
+
+}
