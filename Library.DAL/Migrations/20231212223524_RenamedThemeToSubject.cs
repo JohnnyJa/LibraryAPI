f26b8f78c990469234e963618af69a35c5d8d0f1
Иван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2023-12-13 00:39:59 +0200

    Migrations added

---
+++ Library.DAL/Migrations/20231212223524_RenamedThemeToSubject.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedThemeToSubject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Theme_ThemeId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Theme");
+
+            migrationBuilder.RenameColumn(
+                name: "ThemeId",
+                table: "Books",
+                newName: "SubjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_ThemeId",
+                table: "Books",
+                newName: "IX_Books_SubjectId");
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Subjects_SubjectId",
+                table: "Books",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Subjects_SubjectId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.RenameColumn(
+                name: "SubjectId",
+                table: "Books",
+                newName: "ThemeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_SubjectId",
+                table: "Books",
+                newName: "IX_Books_ThemeId");
+
+            migrationBuilder.CreateTable(
+                name: "Theme",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Theme", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Theme_ThemeId",
+                table: "Books",
+                column: "ThemeId",
+                principalTable: "Theme",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
