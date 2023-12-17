e586ca0796863a4cb006d920f9f568d56f7b608d
Иван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2023-12-17 22:59:16 +0200

    Books Handler added

---
+++ Library.BLL/Handlers/Books/CreateBookRequestHandler.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+using ErrorOr;
+using Library.BLL.Commands.Base;
+using Library.BLL.Requests.Book;
+using Library.BLL.Services.Responses;
+using Library.DAL.Entities;
+using Library.DAL.Repository;
+using Library.DAL.Repository.IRepository;
+using MediatR;
+using MediatR.Wrappers;
+
+namespace Library.BLL.Commands.Books;
+
+public class CreateBookRequestHandler : RequestHandlerBase<CreateBookRequest, BookResponse>
+{
+    private readonly IRepository<Book> _repository;
+    private readonly IMapper _mapper;
+
+    public CreateBookRequestHandler(IMapper mapper, IRepository<Book> repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    protected override async Task<ErrorOr<BookResponse>> HandleInternal(CreateBookRequest request, CancellationToken cancellationToken)
+    {
+        var book = _mapper.Map<Book>(request);
+        await _repository.AddAsync(book, cancellationToken);
+        return  _mapper.Map<BookResponse>(book);
+    }
+}
---
+++ Library.WebApi/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+using AutoMapper;
+using Library.BLL.Requests.Book;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.WebApi.Controllers;
+
+[ApiController]
+[Route("api/books")]
+public class BookController
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public BookController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllBooks()
+    {
+        var response = await _mediator.Send(new GetAllBooksRequest());
+        return new OkObjectResult(response);
+    }
+}
